ab991ca873ce217582560a88112be07ad08c9f6d
Aaron Buachie <aaron.buachiea@gmail.com>
2020-02-24 11:26:26 -0500

    Set different functions for current weather and weekly forecast

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ window.addEventListener("load", () => {
   let lat;
   let temp = document.querySelector(".temp-degree");
   let location = document.querySelector(".location");
-  let time = document.querySelector(".time");
   let weather = document.querySelector(".weather");
   let apparentTemp = document.querySelector(".apparent-degree");
   let weatherIcon = document.querySelector(".icon");
   let weeklyForecast = document.querySelector(".weekly");
 
+  // Convert darksky time to selected day
   function tm(unix_tm) {
     var dt = new Date(unix_tm * 1000);
     day = dt.getDay();
@@ -86,76 +86,79 @@ window.addEventListener("load", () => {
         });
     });
   }
+
+  //Sets icons for weekly weather
   setIconsWeekly = (icon, iconID) => {
-    const skycons = new Skycons({ color: "white" });
+    const skycons = new Skycons({ color: "black" });
     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
 
     skycons.play();
     return skycons.set(iconID, Skycons[currentIcon]);
   };
 
+  //Sets Icon and background for the current day
   setIconsCurrent = (icon, iconID) => {
-    const skycons = new Skycons({ color: "white" });
+    const skycons = new Skycons();
     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
 
     skycons.play();
-    for (let x = 0; x < 1; x++) {
-      switch (currentIcon) {
-        case "CLEAR_DAY":
-          body.style.background = "linear-gradient(to right, #ffe259, #ffa751)";
-          skycons.color = "black";
-          body.style.color = "#000";
-          break;
-        case "CLEAR_NIGHT":
-          body.style.background = "linear-gradient(to right, #41295a, #2F0743)";
-          skycons.color = "white";
-          body.style.color = "#fff";
-          break;
-        case "PARTLY_CLOUDY_DAY":
-          body.style.background = "linear-gradient(to right, #ED4264, #FFEDBC)";
-          skycons.color = "black";
-          body.style.color = "#000";
-          break;
-        case "PARTLY_CLOUDY_NIGHT":
-          body.style.background = "linear-gradient(to right, #5C258D, #4389A2)";
-          skycons.color = "white";
-          body.style.color = "#fff";
-          break;
-        case "CLOUDY":
-          body.style.background = "linear-gradient(to right, #8e9eab, #eef2f3)";
-          skycons.color = "black";
-          body.style.color = "#000";
-          break;
-        case "RAIN":
-          body.style.background =
-            "linear-gradient(to bottom right, #1A2980, #26D0CE)";
-          skycons.color = "white";
-          body.style.color = "#fff";
-          break;
-        case "WIND":
-          body.style.background = "linear-gradient(to right, #acb6e5, #86fde8)";
-          skycons.color = "black";
-          body.style.color = "#000";
-          break;
-        case "SNOW":
-          body.style.background = "linear-gradient(to right, #ECE9E6, #FFFFFF)";
-          skycons.color = "black";
-          body.style.color = "#000";
-          break;
-        case "SLEET":
-          body.style.background = "linear-gradient(to right, #ECE9E6, #FFFFFF)";
-          skycons.color = "black";
-          body.style.color = "#000";
-          break;
-        case "FOG":
-          body.style.background = "linear-gradient(to right, #E6DADA, #274046)";
-          skycons.color = "black";
-          body.style.color = "#000";
-          break;
 
-        default:
-          break;
-      }
+    //Sets background and text color based on weather
+    switch (currentIcon) {
+      case "CLEAR_DAY":
+        body.style.background = "linear-gradient(to right, #ffe259, #ffa751)";
+        skycons.color = "black";
+        body.style.color = "#000";
+        break;
+      case "CLEAR_NIGHT":
+        body.style.background = "linear-gradient(to right, #41295a, #2F0743)";
+        skycons.color = "white";
+        body.style.color = "#fff";
+        break;
+      case "PARTLY_CLOUDY_DAY":
+        body.style.background = "linear-gradient(to right, #ED4264, #FFEDBC)";
+        skycons.color = "black";
+        body.style.color = "#000";
+        break;
+      case "PARTLY_CLOUDY_NIGHT":
+        body.style.background = "linear-gradient(to right, #5C258D, #4389A2)";
+        skycons.color = "white";
+        body.style.color = "#fff";
+        break;
+      case "CLOUDY":
+        body.style.background = "linear-gradient(to right, #8e9eab, #eef2f3)";
+        skycons.color = "black";
+        body.style.color = "#000";
+        break;
+      case "RAIN":
+        body.style.background =
+          "linear-gradient(to bottom right, #1A2980, #26D0CE)";
+        skycons.color = "white";
+        body.style.color = "#fff";
+        break;
+      case "WIND":
+        body.style.background = "linear-gradient(to right, #acb6e5, #86fde8)";
+        skycons.color = "black";
+        body.style.color = "#000";
+        break;
+      case "SNOW":
+        body.style.background = "linear-gradient(to right, #ECE9E6, #FFFFFF)";
+        skycons.color = "black";
+        body.style.color = "#000";
+        break;
+      case "SLEET":
+        body.style.background = "linear-gradient(to right, #ECE9E6, #FFFFFF)";
+        skycons.color = "black";
+        body.style.color = "#000";
+        break;
+      case "FOG":
+        body.style.background = "linear-gradient(to right, #E6DADA, #274046)";
+        skycons.color = "black";
+        body.style.color = "#000";
+        break;
+
+      default:
+        break;
     }
     return skycons.set(iconID, Skycons[currentIcon]);
   };
